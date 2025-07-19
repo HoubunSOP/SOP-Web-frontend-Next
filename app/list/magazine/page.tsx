d63e02c9aab247973d7562086a0d2fc76b2ee059
misaka10843 <misaka10843@outlook.jp>
2025-07-19 18:30:50 +0800

    update

--- app/list/magazine/page.tsx
+++ app/list/magazine/page.tsx
@@ -1,146 +1,16 @@
-'use client'
+import {Suspense} from 'react'
+import content from './content'
 
-import React, { useCallback, useEffect, useRef, useState } from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import { useSearchParams } from 'next/navigation'
-import { Box, LoadingOverlay, Pagination } from '@mantine/core'
-import { useScrollIntoView } from '@mantine/hooks'
-import { Sidebar } from '@/components/Sidebar/Sidebar'
-import { MainColumn } from '@/components/layout/MainColumn'
-import scrollToTop from '@/components/scrollToTop'
-import { MangazineItem } from '@/type/mangazine'
-import { fetchMagazines } from '@/utils/api'
+function contentFallback() {
+    return <>placeholder</>
+}
 
-export default function MagazineListPage() {
-  const [totalPages, setTotalPages] = useState(1)
-  const [magazines, setMagazines] = useState<MangazineItem[]>([])
-  const [currentPage, setCurrentPage] = useState(1)
-  const [visible, toggle] = useState(true)
-  const searchParams = useSearchParams()
-  const { scrollIntoView, targetRef } = useScrollIntoView<HTMLDivElement>()
-  const category_id = searchParams.get('category_id')
-
-  const fetchMagazinesAPI = useCallback(async () => {
-    console.log('Fetching magazines...')
-    try {
-      const data = await fetchMagazines(currentPage, category_id)
-      setMagazines(data.detail.items)
-      setTotalPages(data.detail.total_pages)
-      scrollIntoView()
-    } catch (error) {
-      console.error(error)
-    } finally {
-      toggle(false)
-      scrollToTop()
-    }
-  }, [currentPage, category_id])
-
-  const handlePageChange = (page: number) => {
-    setCurrentPage(page)
-  }
-
-  const isInitialRender = useRef(true)
-
-  useEffect(() => {
-    if (isInitialRender.current) {
-      isInitialRender.current = false
-      return
-    }
-
-    let isMounted = true
-
-    const fetchData = async () => {
-      toggle(true)
-      await fetchMagazinesAPI()
-      if (isMounted) toggle(false)
-    }
-
-    fetchData().then((r) => {})
-
-    return () => {
-      isMounted = false
-    }
-  }, [fetchMagazinesAPI, currentPage, category_id])
-
-  const emptyMagazines = Array.from({ length: 12 }, () => ({
-    id: 0,
-    name: 'Now loading...',
-    publish_date: 'Getting Date...',
-    cover: '/images/now_printing.webp',
-  }))
-
-  const magazinesWithFallback =
-    magazines.length > 0 ? magazines : emptyMagazines
-
-  return (
-    <>
-      <MainColumn>
-        <div
-          className="px-[22px] pt-[18px] border-b-[2px] border-gray-200 overflow-hidden bg-white box-border"
-          ref={targetRef}
-        >
-          <div className="relative mb-0">
-            <h1 className="m-0 flex">
-              <span className="inline-block text-[#242a36] text-base font-bold tracking-wide">
-                <i className="fa-duotone fa-books" />
-                杂志列表
-              </span>
-            </h1>
-          </div>
-        </div>
-        <Box pos="relative">
-          <LoadingOverlay
-            visible={visible}
-            transitionProps={{ transition: 'fade', duration: 10 }}
-            zIndex={1000}
-            overlayProps={{
-              radius: 'sm',
-              blur: 2,
-            }}
-          />
-          <div className="flex flex-wrap margin-[-5px] mt-[10px]">
-            {magazinesWithFallback.map((magazine, i) => (
-              <div
-                key={magazine.id || i}
-                className="comic max-w-none mt-0 mx-[5px] mb-[18px] rounded-md list-none transition-all hover:bg-slate-100 hover:scale-[1.02] ease-in-out"
-                style={{ width: 'calc(100% / 4 - 10px)' }}
-              >
-                <Link href={`/magazine/${magazine.id}`}>
-                  <figure className="rounded-md m-0 overflow-hidden relative">
-                    <span className="relative block h-0 w-full pt-[150%] overflow-hidden">
-                      <Image
-                        alt="manga cover"
-                        className="object-cover !rounded-md h-[90%] w-[90%] -translate-x-1/2 -translate-y-1/2 absolute top-1/2 left-1/2"
-                        height={134}
-                        src={magazine.cover || '/images/now_printing.webp'}
-                        width={96}
-                        loading={'lazy'}
-                        placeholder={'blur'}
-                        blurDataURL={'/images/now_printing.webp'}
-                      />
-                    </span>
-                  </figure>
-                </Link>
-                <p className="text-center whitespace-nowrap text-ellipsis text-[15px] h-[25px] font-normal overflow-hidden">
-                  {magazine.name || 'Now Loading...'}
-                </p>
-                <p className="mt-[8px] text-center whitespace-nowrap text-[#808080] text-[10px] font-normal">
-                  {magazine.publish_date + '发布' || 'Now Loading...'}
-                </p>
-              </div>
-            ))}
-          </div>
-        </Box>
-        <Pagination
-          className="flex justify-center"
-          total={totalPages}
-          value={currentPage}
-          onChange={handlePageChange}
-          mt="sm"
-        />
-      </MainColumn>
-      <Sidebar />
-    </>
-  )
+export default function Page() {
+    return (
+        <>
+            <Suspense fallback={<contentFallback/>}>
+                <content/>
+            </Suspense>
+        </>
+    )
 }
---
+++ app/list/post/content.tsx
@@ -0,0 +1,139 @@
+'use client'
+
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+import Link from 'next/link'
+import { Image, Pagination } from '@mantine/core'
+import { useScrollIntoView } from '@mantine/hooks'
+import { Sidebar } from '@/components/Sidebar/Sidebar'
+import { MainColumn } from '@/components/layout/MainColumn'
+import { useSearchParams } from 'next/navigation'
+import { PostListLoading } from '@/components/index/PostList.loading'
+import { Detail } from '@/type/article'
+import { fetchArticles } from '@/utils/api'
+
+export default function PostListPage() {
+  const searchParams = useSearchParams()
+  const [totalPages, setTotalPages] = useState(1)
+  const [articles, setPosts] = useState<Detail[]>([])
+  const [currentPage, setCurrentPage] = useState(1)
+  const [visible, toggle] = useState(true)
+  const { scrollIntoView, targetRef } = useScrollIntoView<HTMLDivElement>()
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+  // 获取URL中的查询参数
+  const category_id = searchParams.get('category_id')
+
+  const fetchArticlesAPI = useCallback(async () => {
+    setIsLoading(true)
+    console.log('Fetching articles...')
+    try {
+      const data = await fetchArticles(currentPage, category_id)
+      if (data.status !== 200) {
+        console.log('error')
+      }
+      setIsLoading(false)
+      setPosts(data.detail.items)
+      setTotalPages(data.detail.total_pages)
+      scrollIntoView()
+    } catch (error) {
+      console.error(error)
+    } finally {
+      toggle(false)
+      scrollIntoView()
+    }
+  }, [currentPage, category_id])
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page)
+  }
+
+  const isInitialRender = useRef(true)
+
+  useEffect(() => {
+    if (isInitialRender.current) {
+      isInitialRender.current = false
+      return
+    }
+
+    let isMounted = true
+
+    const fetchData = async () => {
+      toggle(true)
+      await fetchArticlesAPI()
+      if (isMounted) toggle(false)
+    }
+
+    fetchData().then((r) => {})
+
+    return () => {
+      isMounted = false
+    }
+  }, [fetchArticlesAPI, currentPage, category_id])
+  return (
+    <>
+      <MainColumn>
+        <div
+          ref={targetRef}
+          className="px-[22px] pt-[18px] border-b-[2px] border-gray-200 overflow-hidden bg-white box-border relative mb-3"
+        >
+          <h1 className="m-0 flex">
+            <span className="inline-block text-gray-900 text-xl font-bold tracking-wide">
+              <i className="fa-duotone fa-list mr-1" />
+              文章列表
+            </span>
+          </h1>
+        </div>
+        <div>
+          {isLoading ? (
+            <PostListLoading />
+          ) : (
+            articles.map((article) => (
+              <Link href={`/post/${article.id}`} key={article.id} passHref>
+                <div className="pt-6 pb-4 px-6 relative rounded-md flex flex-wrap overflow-hidden transition-all hover:bg-slate-100 hover:scale-105 ease-in-out">
+                  <h2
+                    className="overflow-hidden h-16 mr-5 text-md font-medium line-clamp-3"
+                    style={{
+                      width: 'calc(100%-162px)',
+                    }}
+                  >
+                    {article.title}
+                  </h2>
+                  <div className="justify-self-end ml-auto w-30 h-18 md:w-36 md:h-22 rounded-md overflow-hidden relative">
+                    <Image src={article.cover} alt="post cover" />
+                  </div>
+                  <div className="absolute bottom-2.5">
+                    <Link
+                      className="mr-2"
+                      href={{
+                        pathname: '/list/post',
+                        query: { category_id: article.categories[0].id },
+                      }}
+                    >
+                      <span className="text-xs md:text-sm tracking-wide text-gray-500">
+                        <i className="fa-duotone fa-list-ul mr-1"></i>
+                        {article.categories[0].name}
+                      </span>
+                    </Link>
+                    <span className="text-xs md:text-sm tracking-wide text-gray-500">
+                      <i className="fa-duotone fa-calendar-week mr-1"></i>
+                      {article.date}
+                    </span>
+                  </div>
+                </div>
+              </Link>
+            ))
+          )}
+        </div>
+        <Pagination
+          className="flex justify-center mt-10"
+          total={totalPages}
+          value={currentPage}
+          onChange={handlePageChange}
+          color="indigo"
+          radius="md"
+          withEdges
+        />
+      </MainColumn>
+      <Sidebar />
+    </>
+  )
+}
